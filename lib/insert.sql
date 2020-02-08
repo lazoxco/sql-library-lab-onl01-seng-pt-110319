a24a0af887144e09913310c7cd927c5af3dbe43c
Nory Lazo <nlazo@me.com>
2020-02-08 00:48:42 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Hello", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("World", 2, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("Hatchet", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Green Eggs and Ham", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Cat in the Hat", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Alice in Wonderland", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Secret Garden", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("My Friend Leonard", 1999, 1);
+
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "dog", 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ("Bill", "Hello", "alien", 1);
+
+INSERT INTO subgenres (name) VALUES ("Horror");
+INSERT INTO subgenres (name) VALUES ("Comedy");
+
+INSERT INTO authors (name) VALUES ("Dr. Seuss");
+INSERT INTO authors (name) VALUES ("Emily Dickinson");
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
